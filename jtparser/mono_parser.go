4f534008a19db8d714fefc0c4a50f6ff65d72c82
Rishabh Kumar <rishabh.kumar.pro@gmail.com>
2025-09-21 17:48:28 +0530

    Add support for hot query update

--- jtparser/mono_parser.go
+++ jtparser/mono_parser.go
@@ -31,6 +31,18 @@ func NewMonoParser[T any](query string, searchSpace []any) (*MonoParser[T], erro
 	}, nil
 }
 
+func (p *MonoParser[T]) ChangeQuery(newQuery string) error {
+	tk := tokenizer.NewTokenizer(newQuery)
+	nodes, tokErr := tk.Tokenize()
+	if tokErr != nil {
+		return tokErr
+	}
+
+	p.nodes = nodes
+	p.walker.path = "$"
+	return nil
+}
+
 func (p *MonoParser[T]) Parse() (*T, error) {
 	for _, node := range *p.nodes {
 		if p.walker.previousResults == nil {
